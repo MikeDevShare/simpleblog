d6e331eb538bc5469ba968d2a638e42a1a600bf2
MikeDevShare <trellantest@gmail.com>
2016-03-16 12:07:00 +0200

    first commit

--- app/User.php
+++ app/User.php
@@ -3,14 +3,20 @@
 namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements AuthenticatableContract, CanResetPasswordContract
 {
+    use Authenticatable, CanResetPassword;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    protected $table = 'users';
     protected $fillable = [
         'name', 'email', 'password',
     ];
@@ -23,4 +29,32 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function posts()
+    {
+      return $this->hasMany('App\Posts','author_id');
+    }
+    // user has many comments
+    public function comments()
+    {
+      return $this->hasMany('App\Comments','from_user');
+    }
+    public function can_post()
+    {
+      $role = $this->role;
+       if($role == 'author' || $role == 'admin')
+       {
+         return true;
+       }
+       return false;
+    }
+    public function is_admin()
+    {
+        $role = $this->role;
+        if($role == 'admin')
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
